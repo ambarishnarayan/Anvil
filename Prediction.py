06ad3bfa25f2907466667cd77c6c6d146e204e3d
Ambarish Narayan <32060433+ambarishnarayan@users.noreply.github.com>
2024-02-16 18:10:27 -0600

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -104,7 +104,7 @@ def predict(model: str, file):
            print("vit")
            df = np.expand_dims(im_df, axis=0)
            df = preprocess_data(df, 7, 7)[0]
-           pos_feed = np.array(49)
+           pos_feed = np.array([list(range(49))])
            df = np.expand_dims(df, axis=0)
            pos_feed = np.expand_dims(pos_feed, axis=0)
            print(df.shape, pos_feed.shape)
